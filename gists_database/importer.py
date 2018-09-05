4ef0e09944a1cc9755591130029f0ed94be0ecb2
Connor K <vckelly@gmail.com>
2018-09-05 22:32:06 +0000

    Gists Project

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,41 @@
 import requests
 
+URL_TEMPLATE = 'https://api.github.com/users/{username}/gists'
+
+SQL_QUERY = """
+            INSERT into gists("github_id", "html_url", "git_pull_url",
+            "git_push_url", "commits_url",
+            "forks_url", "public", "created_at",
+            "updated_at", "comments", "comments_url")
+            
+            VALUES (:github_id, :html_url, :git_pull_url,
+            :git_push_url, :commits_url, :forks_url,
+            :public, :created_at, :updated_at,
+            :comments, :comments_url
+);"""
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    resp = requests.get(URL_TEMPLATE.format(username=username))
+    resp.raise_for_status()
+    
+    gists = resp.json()
+    
+    for gist in gists:
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url'],
+        }
+        
+        db.execute(SQL_QUERY, params)
+        if commit:
+            db.commit()
+
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,33 @@
 from .models import Gist
 
+
 def search_gists(db_connection, **kwargs):
-    pass
+    SQL_QUERY = 'SELECT * from gists'
+    git_id = False
+    create = False
+    if 'errormessage' in kwargs:
+        pass
+    
+    if 'github_id' in kwargs:
+        SQL_QUERY += ' WHERE github_id = ?'
+        git_id = True
+    
+    if 'created_at' in kwargs:
+        if git_id:
+            SQL_QUERY += ' AND WHERE created_at = ?'
+        else:
+            SQL_QUERY += ' WHERE created_at = ?' 
+        
+        create = True
+    
+        
+    params = [vals for vals in kwargs.values()]
+    
+    if len(params) < 1:
+        cursor = db_connection.execute(SQL_QUERY)
+
+    else:
+        cursor = db_connection.execute(SQL_QUERY, params)
+        
+    return cursor.fetchall()
+        
